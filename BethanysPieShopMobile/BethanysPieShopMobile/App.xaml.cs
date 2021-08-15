4b05fcbb21a6c80fa08f6e3fc8a7743b21d491dd
AzureAD\LaxmiJammula <laxmi.jammula@carexm.com>
2021-08-15 12:22:26 -0600

    first commit. This project has only MApp code

---
+++ BethanysPieShopMobile/BethanysPieShopMobile/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace BethanysPieShopMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            bool alreadyRegistered = false;
+            //MainPage = new LoginView();
+            //MainPage = new RegisterView();
+            //MainPage = new ContactView();
+            //MainPage = new SettingsView();
+            //MainPage = new WebPageView();
+            //MainPage = new PieDetailView(); 
+            //MainPage = new PieOverview();
+
+            if (alreadyRegistered)
+                MainPage = new MainView();
+            else
+                MainPage = new NavigationPage(new LoginView());
+
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ BethanysPieShopMobile/BethanysPieShopMobile/MainView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BethanysPieShopMobile.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BethanysPieShopMobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : MasterDetailPage
+	{
+		public MainView ()
+		{
+			InitializeComponent ();
+
+		    MasterView.NavigationListView.ItemSelected += NavigationListView_ItemSelected;
+        }
+
+	    private void NavigationListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+	    {
+	        if (e.SelectedItem is MasterNavigationItem item)
+	        {
+	            Detail = new NavigationPage((Page)Activator.CreateInstance(item.Target));
+	            MasterView.NavigationListView.SelectedItem = null;
+	            IsPresented = false;
+	        }
+	    }
+    }
+}
