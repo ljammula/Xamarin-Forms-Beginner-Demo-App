8b1c4e2af60ca82ba23092a21d4f05bb11758141
AzureAD\LaxmiJammula <laxmi.jammula@carexm.com>
2021-08-15 12:36:29 -0600

    adding more resources, projects I created or used

---
+++ GettingStartedApps/FirstAwesomeXmApp/FirstAwesomeXmApp/FirstAwesomeXmApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace FirstAwesomeXmApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int _count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            _count++;
+            ((Button)sender).Text = $"You clicked {_count} times.";
+        }
+    }
+}
---
+++ GettingStartedApps/Notes/Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
